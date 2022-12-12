5519c6792cff93b9dd44153381acf946c7cd1af5
yinzhenyu <sharkrain@outlook.com>
2022-12-12 15:54:49 +0800

    test: add type test case

--- test/core-type.test.ts
+++ test/core-type.test.ts
@@ -1,3 +1,5 @@
+import type { Equal, IsFalse, IsTrue } from "../util/type-util"
+import type { Extract200JSON } from "../src/index"
 import type {
   components,
   paths,
@@ -15,3 +17,29 @@ declare module "../src/index" {
 }
 
 import { createHTTPClient } from "../src/index"
+
+async function getPets() {
+  return await createHTTPClient().get("/pets")
+}
+async function postPet() {
+  return await createHTTPClient().post("/pets", { name: "", tag: "" })
+}
+
+async function deletePet() {
+  return await createHTTPClient().delete("/pets/{id}", { params: { id: 1 } })
+}
+
+type cases = [
+  Equal<
+    Awaited<ReturnType<typeof getPets>>["data"],
+    Extract200JSON<"get", "/pets">
+  >,
+  Equal<
+    Awaited<ReturnType<typeof postPet>>["data"],
+    Extract200JSON<"post", "/pets">
+  >,
+  Equal<
+    Awaited<ReturnType<typeof deletePet>>["data"],
+    Extract200JSON<"delete", "/pets/{id}">
+  >,
+]
