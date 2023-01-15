1ab00bb0be371c187e0394cc20629045ec5ab007
yinzhenyu-su <sharkrain@outlook.com>
2023-01-15 16:48:09 +0800

    refactor: use import alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,18 @@
 ///<reference types="vitest"/>
 import { defineConfig } from "vitest/config"
+import path from "path"
 
 export default defineConfig({
+  root: ".",
+  resolve: {
+    alias: {
+      "@/src": path.resolve(__dirname, "./src"),
+      "@/util": path.resolve(__dirname, "./util"),
+      "@/test": path.resolve(__dirname, "./test"),
+    },
+  },
   test: {
-    exclude: ["test/core-type.test.ts"],
+    include: ["test/index.test.ts"],
+    exclude: ["test/core-type.test.ts", "node_modules/**/*"],
   },
 })
