a4091b611e6a89742c00a42102cf9823b45dac49
yinzhenyu <sharkrain@outlook.com>
2024-11-19 16:11:02 +0800

    feat: 支持从环境变量或命令行参数读取 jsonSchemaPath

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -2,17 +2,19 @@
 import { getDefinition } from "../outdir/api-meta-init.js"
 
 const argv = process.argv.slice(2)
-if (!argv.length) {
+const envJsonSchemaPath = process.env.API_TYPING_JSON_SCHEMA_PATH
+if (!envJsonSchemaPath && !argv.length) {
   console.log(`info: please define a jsonSchemaPath.\n
   Usage: get-types <jsonSchemaPath> <definitionPath> \n
   Example: \n
-    get-types ./schema.json ./types.d.ts\n
-    get-types http://test.com/schema.json ./types.d.ts\n`)
+    get-types ./schema.json ./api-typing-meta.d.ts\n
+    get-types http://test.com/schema.json ./api-typing-meta.d.ts\n`)
   process.exit()
 }
 async function run() {
   return await getDefinition({
-    jsonSchemaPath: argv[0],
+    // read from env or argv
+    jsonSchemaPath: argv[0] || envJsonSchemaPath,
     definitionPath: argv[1],
   })
 }
